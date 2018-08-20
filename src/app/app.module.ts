c271cfc2f21748496aebed802fd8caf76f09bc98
Selin Denise Acar <selin@Selins-Mac.local>
2018-08-20 17:28:35 -0400

    initial setup and firebase working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { MenuComponent } from './menu/menu.component';
+import { ProjectDetailComponent } from './project-detail/project-detail.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ProjectsComponent,
+    MenuComponent,
+    ProjectDetailComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
